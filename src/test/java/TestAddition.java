d14c75ffa95af15b8d7ac68838d9380da6c44e2a
nachogarrone <nachogarrone@gmail.com>
2016-08-29 22:40:31 -0300

    JUnit

---
+++ src/test/java/TestAddition.java
@@ -0,0 +1,13 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by nachogarrone on 29/8/16.
+ */
+public class TestAddition {
+
+    @Test
+    public void testAdd(){
+        Assert.assertTrue(true);
+    }
+}
